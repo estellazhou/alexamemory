df6906569d7f027fecf38b675ca3a52cd4f39904
estellazhou <145620388+estellazhou@users.noreply.github.com>
2024-02-18 02:02:48 -0800

    Update people.js

--- memory/lambda/people.js
+++ memory/lambda/people.js
@@ -1,6 +1,6 @@
 // Define the data directly in your Lambda function
 const data = `
-Alice:me:80:California:when i was little we jumped
+Alice:me:80:California:when i was little we jumped rope.
 Jane:daughter:50:Nevada:That morning, on the way to Jane's 2nd grade class, right after a quick rain shower, something special happened. We spotted a double rainbow stretching across the sky. Just as we stood there looking at it, a monarch butterfly zipped by us. Jane's excitement was through the roof, and she chatted about it nonstop all the way to school. It was a simple day that turned into an unforgettable one, just because of that moment.
 John:son:52:Florida:Your son John grew up watching you and your husband dance to La Vie En Rose by Edith Piaf in front of the fireplace on snowy evenings. When he married the love of his life, the two had their first dance to the same song, carrying on the tradition you started.
 `;
